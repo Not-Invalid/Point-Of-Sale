3745ef6ffa09de6e17fa6bf04bf49b1691379f2d
RaffDev31 <raffli4000@hotmail.com>
2024-07-25 14:38:19 +0700

    feat: display brand and category in invoice

--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -26,22 +26,25 @@
     </div>
     <p>Invoice Id: {{ $invoiceData['invoiceId'] }}</p>
     <p>Transaction date: {{ $invoiceData['transactionDate'] }}</p>
+
     <table style="width:100%">
         <tr>
-            <th>No</th>
             <th>Product Name</th>
+            <th>Brand</th>
+            <th>Category</th>
             <th>Quantity</th>
             <th>Price</th>
         </tr>
         <tr>
-            <td>1</td>
             <td>{{ $invoiceData['productName'] }}</td>
+            <td>{{ $invoiceData['id_brand'] }}</td>
+            <td>{{ $invoiceData['id_category'] }}</td>
             <td>{{ $invoiceData['quantity'] }}</td>
             <td>{{ formatRupiah($invoiceData['unitPrice']) }}</td>
         </tr>
         <tr>
-            <td colspan="3" style="text-align:right;">Total</td>
-            <td>{{ formatRupiah( $invoiceData['total'] )}}</td>
+            <td colspan="4" style="text-align:right;">Total</td>
+            <td>{{ formatRupiah($invoiceData['total']) }}</td>
         </tr>
     </table>
 </body>
