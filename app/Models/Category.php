3bb5154620a37920d2779f3a25982eefa0ef036a
RaffDev31 <raffli4000@hotmail.com>
2024-07-31 12:53:58 +0700

    feat: add custom primary key settings to Category  model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,8 +10,13 @@ class Category extends Model
     use HasFactory;
 
     protected $fillable = [
+        'category_id',
         'category_name'
     ];
 
     protected $table = 'categories';
+    protected $primaryKey = 'category_id';
+    public $incrementing = false; 
+    protected $keyType = 'string'; 
+
 }
