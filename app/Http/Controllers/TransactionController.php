773329a61ae4ac0ee2b954eb04cd8b2e6853af08
RaffDev31 <raffli4000@hotmail.com>
2024-07-31 11:04:14 +0700

    Refactor TransactionController
    1. Format unit price and total to follow the selected currency.
    2. Change transaction number format from TRX-XXX to TRA/d/m/Y/X.
    3. Disable exchange rates feature.

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -4,49 +4,56 @@ namespace App\Http\Controllers;
 
 use App\Models\Transaction;
 use App\Models\Product;
+use App\Models\Currency;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
     public function index(Request $request)
     {
-        $currency = $request->session()->get('selectedCurrency', 'IDR');
-        $exchangeRates = $request->session()->get('exchangeRates', []);
+        $transactions = Transaction::all();
+        $currencySetting = Currency::first();
+        $selectedCurrency = $currencySetting ? $currencySetting->currency_code : 'IDR';
+        // $exchangeRates = $request->session()->get('exchangeRates', []);
         
-        $rate = $exchangeRates[$currency] ?? 1;
+        // $rate = $exchangeRates[$currency] ?? 1;
 
         $transactions = Transaction::orderBy('created_at', 'DESC');
 
         if ($request->has('search')) {
             $search = $request->query('search');
-            $transactions->where('transaction_number', 'like', '%' . $search . '%');
+            $transactions->where('transaction_number', 'like', '%' . $search . '%')->orWhere('product_name', 'like', '%' . $search . '%');
         }
 
         $transactions = $transactions->paginate(10);
         $products = Product::all();
 
-        foreach ($transactions as $transaction) {
-            $transaction->unit_price = $transaction->unit_price * $rate;
-            $transaction->total = $transaction->total * $rate;
-        }
+        // foreach ($transactions as $transaction) {
+        //     $transaction->unit_price = $transaction->unit_price * $rate;
+        //     $transaction->total = $transaction->total * $rate;
+        // }
 
-        return view('kasir.transaction.index', compact('transactions', 'products', 'currency'));
+        return view('kasir.transaction.index', compact('transactions', 'products', 'selectedCurrency'));
     }
 
-    public function indexAdmin()
+    public function indexAdmin(Request $request)
     {
         $currency = session('selectedCurrency', 'IDR');
-        $exchangeRates = session('exchangeRates', []);
+        // $exchangeRates = session('exchangeRates', []);
         
-        $rate = $exchangeRates[$currency] ?? 1;
+        // $rate = $exchangeRates[$currency] ?? 1;
 
         $transactions = Transaction::orderBy('created_at', 'DESC')->paginate(10);
+        if ($request->has('search')) {
+            $search = $request->query('search');
+            $transactions->where('transaction_number', 'like', '%' . $search . '%')->orWhere('product_name', 'like', '%' . $search . '%');
+        }
         $products = Product::all();
 
-        foreach ($transactions as $transaction) {
-            $transaction->unit_price = $transaction->unit_price * $rate;
-            $transaction->total = $transaction->total * $rate;
-        }
+        // foreach ($transactions as $transaction) {
+        //     $transaction->unit_price = $transaction->unit_price * $rate;
+        //     $transaction->total = $transaction->total * $rate;
+        // }
 
         return view('admin.transaction.index', compact('transactions', 'products', 'currency'));
     }
@@ -55,40 +62,48 @@ class TransactionController extends Controller
     {
         $products = Product::all();
         $transactions = Transaction::orderBy('created_at', 'DESC')->paginate(10);
-        return view('kasir.transaction.index', compact('products', 'transactions'));
+        return view('kasir.transaction.create', compact('products', 'transactions'));
     }
 
     public function store(Request $request)
     {
         $request->validate([
-            'product_id' => 'required|exists:products,id',
+            'product_id' => 'required|exists:products,product_id',
             'qty' => 'required|integer|min:1',
         ]);
 
-        $product = Product::find($request->product_id);
+        $products = Product::find($request->product_id);
 
-        if ($request->qty > $product->stock) {
-            return redirect()->route('kasir.transaction.add')
+        if ($request->qty > $products->stock) {
+            return redirect()->route('transaction-create')
                                 ->with('error', 'Quantity is more than stock.');
         }
 
         $currency = session('selectedCurrency', 'IDR');
-        $exchangeRates = session('exchangeRates', []);
-        $rate = $exchangeRates[$currency] ?? 1;
+        // $exchangeRates = session('exchangeRates', []);
+        // $rate = $exchangeRates[$currency] ?? 1;
+
+
+        $today = date('d/m/Y');
+
+        $todayTransactionCount = Transaction::whereDate('created_at', now()->format('Y-m-d'))->count();
+        $transactionNumberSuffix = $todayTransactionCount + 1;
+
+        $transactionNumber = 'TRA/' . $today . '/' . $transactionNumberSuffix;
 
         $transaction = new Transaction();
-        $transaction->transaction_number = 'TRX-' . uniqid();
-        $transaction->product_name = $product->product_name;
-        $transaction->unit_price = $product->unit_price * $rate;
-        $transaction->id_brand = $product->id_brand;
-        $transaction->id_category = $product->id_category;
+        $transaction->transaction_number = $transactionNumber;
+        $transaction->product_name = $products->product_name;
+        $transaction->unit_price = $products->unit_price;
+        $transaction->id_brand = $products->id_brand;
+        $transaction->id_category = $products->id_category;
         $transaction->qty = $request->qty;
         $transaction->total = $transaction->unit_price * $transaction->qty;
 
         $transaction->save();
 
-        $product->stock -= $request->qty;
-        $product->save();
+        $products->stock -= $request->qty;
+        $products->save();
 
         return redirect()->route('kasir.transaction.index')->with('success', 'Transaction added successfully.');
     }
