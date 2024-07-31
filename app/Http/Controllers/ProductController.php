e2173df7b32d56d792cec323995caaf8ac9b4d49
RaffDev31 <raffli4000@hotmail.com>
2024-07-31 11:01:43 +0700

    Refactor ProductController:
    1. Update unit price formatting to follow the selected currency.
    2. Add show function to display product details.
    3. Disable feature to use exchange rates for currency conversion.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,38 +6,51 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Brand;
 use App\Models\Category;
+use Illuminate\Support\Facades\Log;
+
 
 class ProductController extends Controller
 {
     public function index(Request $request)
     {
-        $selectedCurrency = session('selectedCurrency', 'IDR');
-        $exchangeRates = session('exchangeRates', []);
+        $currency = $request->session()->get('selectedCurrency', 'IDR');
+        // $selectedCurrency = session('selectedCurrency', 'IDR');
+        // $exchangeRates = session('exchangeRates', []);
         
-        $products = Product::with('brand', 'categories')->orderBy('id', 'DESC')->paginate(10);
+        $products = Product::with('brand', 'categories')->orderBy('product_id', 'DESC')->paginate(10);
         $brands = Brand::all();
         $categories = Category::all();
         
         if ($request->has('search')) {
             $search = $request->query('search');
-            $products->where('product_name', 'like', '%' . $search . '%');
+            $products->where('product_id', 'like', '%' . $search . '%')->orWhere('product_name', 'like', '%' . $search . '%');
         }
 
-        return view('admin.products.index', compact('products', 'brands', 'categories', 'selectedCurrency', 'exchangeRates'));
+        return view('admin.products.index', compact('products', 'brands', 'categories', 'currency'));
     }
 
-    public function show($id)
+    public function show(Request $request, $product_id)
     {
-        $selectedCurrency = session('selectedCurrency', 'IDR');
-        $exchangeRates = session('exchangeRates', []);
+        // $selectedCurrency = session('selectedCurrency', 'IDR');
+        // $exchangeRates = session('exchangeRates', []);
 
-        $product = Product::with(['brand', 'categories'])->find($id);
-
+        $product = Product::with(['brand', 'categories'])->find($product_id);
+        $brands = Brand::all();
+        $categories = Category::all();
+        $currency = $request->session()->get('selectedCurrency', 'IDR');
         if (!$product) {
             return redirect()->route('admin.products.index')->with('error', 'Product not found.');
         }
 
-        return view('admin.products.detail', compact('product', 'selectedCurrency', 'exchangeRates'));
+        return view('admin.products.show', compact('product', 'currency', 'brands', 'categories'));
+    }
+
+    public function add()
+    {
+        $product = Product::with(['brand', 'categories']);
+        $brands = Brand::all();
+        $categories = Category::all();
+        return view('admin.products.create', compact('product', 'brands', 'categories'));
     }
 
     public function store(Request $request)
@@ -47,12 +60,22 @@ class ProductController extends Controller
             'desc_product' => 'required',
             'stock' => 'required|integer',
             'unit_price' => 'required|numeric',
-            'id_brand' => 'required|exists:brand,id',
-            'id_category' => 'required|exists:categories,id',
-            'product_image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+            'id_brand' => 'required|exists:brand,brand_id',
+            'id_category' => 'required|exists:categories,category_id',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
         ]);
 
+        $lastProduct = Product::orderBy('product_id', 'DESC')->first();
+        if ($lastProduct) {
+            $lastId = intval(substr($lastProduct->product_id, 4));
+            $newId = 'PRO-' . str_pad($lastId + 1, 3, '0', STR_PAD_LEFT);
+        } else {
+            $newId = 'PRO-001';
+        }
+
+
         $product = new Product;
+        $product->product_id = $newId;
         $product->product_name = $request->product_name;
         $product->desc_product = $request->desc_product;
         $product->stock = $request->stock;
@@ -60,30 +83,32 @@ class ProductController extends Controller
         $product->id_brand = $request->id_brand;
         $product->id_category = $request->id_category;
 
-        if($request->hasFile('product_image')){
-            $imageName = time().'.'.$request->product_image->extension();  
-            $request->product_image->move(public_path('products'), $imageName);
-            $product->product_image = 'products/'.$imageName;
+        if ($request->hasFile('product_image')) {
+            $image = $request->file('product_image');
+            $imageName = time() . '_' . $image->getClientOriginalName();
+            $image->move(public_path('products'), $imageName);
+            $product->product_image = 'products/' . $imageName;
         }
+
 
         $product->save();
 
         return redirect()->route('admin.products.index')->with('success', 'Product added successfully.');
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, $product_id)
     {
         $request->validate([
-            'product_name' => 'required|unique:products,product_name,'.$id,
+            'product_name' => 'required|unique:products,product_name,' . $product_id . ',product_id',
             'desc_product' => 'required',
             'stock' => 'required|integer',
             'unit_price' => 'required|numeric',
-            'id_brand' => 'required|exists:brand,id',
-            'id_category' => 'required|exists:categories,id',
-            'product_image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+            'id_brand' => 'required|exists:brand,brand_id',
+            'id_category' => 'required|exists:categories,category_id',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
         ]);
 
-        $product = Product::find($id);
+        $product = Product::where('product_id', $product_id)->firstOrFail();
         $product->product_name = $request->product_name;
         $product->desc_product = $request->desc_product;
         $product->stock = $request->stock;
@@ -91,20 +116,21 @@ class ProductController extends Controller
         $product->id_brand = $request->id_brand;
         $product->id_category = $request->id_category;
 
-        if($request->hasFile('product_image')){
-            $imageName = time().'.'.$request->product_image->extension();  
-            $request->product_image->move(public_path('images'), $imageName);
-            $product->product_image = 'images/'.$imageName;
+        if ($request->hasFile('product_image')) {
+            $image = $request->file('product_image');
+            $imageName = time() . '_' . $image->getClientOriginalName();
+            $image->move(public_path('products'), $imageName);
+            $product->product_image = 'products/' . $imageName;
         }
-
         $product->save();
 
         return redirect()->route('admin.products.index')->with('success', 'Product updated successfully.');
     }
 
-    public function destroy($id)
+
+    public function destroy($product_id)
     {
-        $product = Product::find($id);
+        $product = Product::find($product_id);
         if ($product->product_image && file_exists(public_path($product->product_image))) {
             unlink(public_path($product->product_image));
         }
