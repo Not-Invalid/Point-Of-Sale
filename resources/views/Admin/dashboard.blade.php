47020a09125da88c74fb892afefd8e506254626b
RaffDev31 <raffli4000@hotmail.com>
2024-08-06 14:38:12 +0700

    feat: added responsive design for dashboard

--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -4,8 +4,8 @@
 
 <style>
     canvas {
-        width: 66rem !important;
-        height: 33rem !important;
+        width: 100% !important;
+        height: auto !important;
     }
 </style>
 
@@ -23,10 +23,9 @@
 @endif
 
 <h1 class="font-medium text-2xl">Dashboard</h1>
-<div class="bg-white h-[151px] shadow-none drop-shadow-lg rounded-3xl mt-10 p-6 flex xl:space-x-20z lg:space-x-10 justify-center items-center">
-
-    <div class="flex items-center space-x-8">
-        <div class="bg-gradient-to-b from-green-200 to-green-50  rounded-full w-16 h-16 flex items-center justify-center">
+<div class="bg-white shadow-none drop-shadow-lg rounded-3xl mt-10 p-6 block justify-center items-center space-y-4 md:space-y-10 md:flex lg:flex lg:space-x-10">
+    <div class="flex items-center space-x-8 mt-8 h-full">
+        <div class="bg-gradient-to-b from-green-200 to-green-50 rounded-full w-16 h-16 flex items-center justify-center">
             <i class="fas fa-box fa-2x text-green-500"></i>
         </div>
         <div>
@@ -35,10 +34,9 @@
         </div>
     </div>
 
-
     <div class="border-r border-gray-200"></div>
-    <div class="flex items-center space-x-8">
-        <div class="bg-gradient-to-b from-green-200 to-green-50  rounded-full w-16 h-16 flex items-center justify-center">
+    <div class="flex items-center space-x-8 h-full">
+        <div class="bg-gradient-to-b from-green-200 to-green-50 rounded-full w-16 h-16 flex items-center justify-center">
             <i class="fa-solid fa-chart-line fa-2x text-green-600"></i>
         </div>
         <div>
@@ -49,21 +47,21 @@
 
     <div class="border-r border-gray-200"></div>
 
-    <div class="flex items-center space-x-8">
-        <div class="bg-gradient-to-b from-green-200 to-green-50  rounded-full w-16 h-16 flex items-center justify-center">
+    <div class="flex items-center space-x-8 h-full">
+        <div class="bg-gradient-to-b from-green-200 to-green-50 rounded-full w-16 h-16 flex items-center justify-center">
             <i class="fa-solid fa-dollar-sign fa-2x text-green-600"></i>
         </div>
         <div>
             <div class="text-[#272626] font-light text-[14px]">Total Pemasukan</div>
-            <div class="text-2xl font-semibold text-gray-800">{{ formatRupiah($income)}}</div>
+            <div class="text-2xl font-semibold text-gray-800">{{$income}}</div>
         </div>
     </div>
 </div>
 
 
 {{--Chart--}}
-<div class="bg-white h-[560px] shadow-none drop-shadow-lg rounded-xl mt-5">
-    <canvas id="transactionsChart" class="ml-24 items-center"></canvas>
+<div class="bg-white shadow-none drop-shadow-lg rounded-xl mt-5">
+    <canvas id="transactionsChart" class="w-full h-full"></canvas>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
@@ -87,6 +85,8 @@
                 }]
             },
             options: {
+                responsive: true,
+                maintainAspectRatio: false,
                 scales: {
                     y: {
                         beginAtZero: true,
