f538cccfb771835c595e40029030e98234bd3b71
RaffDev31 <raffli4000@hotmail.com>
2024-08-06 13:59:40 +0700

    refactor: change condition for Image Preview

--- public/js/previewImage.js
+++ public/js/previewImage.js
@@ -1,14 +1,22 @@
-document.getElementById('dropzone-file').addEventListener('change', function(event) {
-    const file = event.target.files[0];
-    if (file) {
-        const reader = new FileReader();
+document.addEventListener('DOMContentLoaded', function() {
+    const fileInput = document.getElementById('dropzone-file');
+    const previewImage = document.getElementById('preview-image');
+    const uploadText = document.getElementById('upload-text');
+    const uploadIcon = document.getElementById('upload-icon');
 
-        reader.onload = function(e) {
-            document.getElementById('preview-image').src = e.target.result;
+    fileInput.addEventListener('change', function(event) {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+
+            reader.onload = function(e) {
+                previewImage.src = e.target.result;
+                previewImage.classList.remove('hidden');
+                uploadText.classList.add('hidden'); 
+                uploadIcon.classList.add('hidden');
+            };
+
+            reader.readAsDataURL(file);
         }
-
-        reader.readAsDataURL(file);
-
-        document.getElementById('file-name').innerText = `File: ${file.name}`;
-    }
+    });
 });
