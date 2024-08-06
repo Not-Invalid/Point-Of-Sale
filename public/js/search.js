e3ad4163136dc4ee77d2ec66a3ecfba6b529b2fb
RaffDev31 <raffli4000@hotmail.com>
2024-08-06 14:00:53 +0700

    feat: add new filterTable for stock management

--- public/js/search.js
+++ public/js/search.js
@@ -1,22 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     const searchInput = document.getElementById('search');
 
-    const filterTable = (tableBodyId, columnIndex) => {
+    const filterTable = (tableBodyId, columnIndices) => {
         const tableBody = document.getElementById(tableBodyId);
 
         searchInput.addEventListener('input', function() {
             const searchText = searchInput.value.toLowerCase();
 
             Array.from(tableBody.children).forEach(row => {
-                const cellText = row.children[columnIndex].textContent.toLowerCase();
-                row.style.display = cellText.includes(searchText) ? '' : 'none';
+                const matches = columnIndices.some(index => 
+                    row.children[index].textContent.toLowerCase().includes(searchText)
+                );
+                row.style.display = matches ? '' : 'none';
             });
         });
     };
 
     filterTable('user-table-body', 1);     
-    filterTable('product-table-body', 1);  
-    filterTable('brand-table-body', 1);    
-    filterTable('category-table-body', 1); 
-    filterTable('transactions-table-body', 0); 
+    filterTable('product-table-body', [1, 2]);  
+    filterTable('brand-table-body', [1, 2]);  
+    filterTable('category-table-body', [1, 2]); 
+    filterTable('transactions-table-body', [1,2]); 
+    filterTable('stok-management-table-body', [1]); 
 });
