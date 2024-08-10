2da08647b4ab8d72fa68b2dff5412a1221b7ccf0
RaffDev31 <raffli4000@hotmail.com>
2024-08-10 10:53:33 +0700

    feat: add new seeders for currency

---
+++ database/seeders/CurrencySeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Currency;
+
+class CurrencySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Currency::create([
+            'currency_code' => 'IDR'
+        ]);
+    }
+}
