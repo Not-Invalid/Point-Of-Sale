4a25a0b53c009b7a73f9d57e257abf501e5443ce
RaffDev31 <raffli4000@hotmail.com>
2024-08-12 16:47:36 +0700

    feat(migration receiving_notes): add remarks_references column

---
+++ database/migrations/2024_08_02_023357_create_receiving_notes_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('receiving_notes', function (Blueprint $table) {
+            $table->id();
+            $table->date('input_date');
+            $table->string('product_id');
+            $table->string('id_brand');
+            $table->string('id_category');
+            $table->integer('quantity');
+            $table->string('description')->nullable();
+            $table->string('remarks_references')->nullable();
+            $table->timestamps();
+
+            $table->foreign('product_id')->references('product_id')->on('products')->onDelete('cascade');
+            $table->foreign('id_brand')->references('brand_id')->on('brand')->onDelete('cascade');
+            $table->foreign('id_category')->references('category_id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('receiving_notes', function (Blueprint $table) {
+            $table->dropForeign(['product_id']);
+            $table->dropForeign(['id_brand']);
+            $table->dropForeign(['id_category']);
+        });
+
+        Schema::dropIfExists('receiving_notes');
+    }
+};
