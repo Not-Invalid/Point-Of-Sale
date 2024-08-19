c19b2d48fc742a1873fbd7366cfcfcd07a364d3d
RaffDev31 <raffli4000@hotmail.com>
2024-08-19 11:24:03 +0700

    feat(migration): add new column for receiver and references

--- database/migrations/2024_08_02_023357_create_receiving_notes_table.php
+++ database/migrations/2024_08_02_023357_create_receiving_notes_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('receiving_notes', function (Blueprint $table) {
             $table->id();
             $table->date('input_date');
+            $table->string('receiver');
             $table->string('product_id');
             $table->string('id_brand');
             $table->string('id_category');
